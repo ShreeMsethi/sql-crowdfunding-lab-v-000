d164d0cd367e894ef633d69cf0e551747728d68e
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-10-08 19:46:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,9 +36,9 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   # "Write your SQL query Here"
-  SELECT name, SUM(amount) FROM projects 
+  "SELECT name, SUM(amount) FROM projects 
   INNER JOIN pledges ON projects.id = pledges.project_id 
-  WHERE category = 
+  WHERE category = 'music';""
   
   
 end

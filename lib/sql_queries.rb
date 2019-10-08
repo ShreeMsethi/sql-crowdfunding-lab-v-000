8cc96de312badbf12d7da413295940405f81a2f2
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2019-10-08 20:00:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,9 +26,9 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   
   "SELECT name, SUM(amount) FROM users 
-    INNER JOIN pledges ON users.id = pledges.user_id 
-    ORDER BY amount ASC
+    JOIN pledges ON users.id = pledges.user_id 
     GROUP BY users.name
+    ORDER BY amount ASC
     HAVING SUM(amount)
     ;"
     
